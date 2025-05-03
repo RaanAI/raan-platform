cf6e74bf038eecd34c74f5ed15d2c0ade98a6caa
RaanAI <raan.aicompany@gmail.com>
2025-05-04 00:08:22 +0300

    remove tests

--- backend/tests/test_dify_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test_dify_api():
-    url = "http://localhost:5001/api/status"
-    response = requests.get(url)
-    assert response.status_code == 200, "Dify API is not reachable"
